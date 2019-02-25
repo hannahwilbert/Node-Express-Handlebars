479b92792bc9aae15c8a25cc4c1e56161e5de1f4
hannahwilbert <hannahclaire230@yahoo.com>
2019-02-24 17:24:30 -0800

    moved static

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 // Dependencies
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
 var PORT = process.env.PORT || 3000; 
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
